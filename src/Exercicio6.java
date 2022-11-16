cad0f2668e3ff389116c0b6f381a8dfccd6c04c1
Gcolin1 <guilhermecolin09@gmail.com>
2022-11-16 13:51:42 -0300

    exercicios JAVA

--- src/Exercicio6.java
+++ src/Exercicio6.java
@@ -4,18 +4,26 @@ public class Exercicio6 {
     public static void main(String[] args){
         Scanner scannerLargura = new Scanner(System.in);
         System.out.println("Digite a largura");
-        double largura = scannerLargura.nextInt();
+        double largura = scannerLargura.nextDouble();
 
         Scanner scannerComprimento = new Scanner(System.in);
         System.out.println("Digite o comprimento");
-        double comprimento = scannerComprimento.nextInt();
+        double comprimento = scannerComprimento.nextDouble();
 
-        Scanner scannerProfundidade = new Scanner(System.in);
-        System.out.println("Digite a profundidade");
-        double profundidade = scannerProfundidade.nextInt();
+        Scanner scannerProfundidadeInicial = new Scanner(System.in);
+        System.out.println("Digite a profundidade inicial");
+        double profundidadeInicial = scannerProfundidadeInicial.nextDouble();
+
+        Scanner scannerProfundidadeFinal = new Scanner(System.in);
+        System.out.println("Digite a profundidade final");
+        double profundidadeFinal = scannerProfundidadeFinal.nextDouble();
+
         double capacidade;
+        double profundidadeMedia;
 
-        capacidade = (largura * comprimento * profundidade) * 1000;
+        profundidadeMedia = (profundidadeInicial + profundidadeFinal) / 2;
+
+        capacidade = (largura * comprimento * profundidadeMedia) * 1000;
 
         System.out.println("Essa piscina comporta " + capacidade + " litros de água");
 
