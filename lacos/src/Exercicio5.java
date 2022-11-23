057d42af3b539cef11bca493c6272b22b2deff2f
Gcolin1 <guilhermecolin09@gmail.com>
2022-11-23 16:00:33 -0300

    exercicios laços 5

--- lacos/src/Exercicio5.java
+++ lacos/src/Exercicio5.java
@@ -1,3 +1,24 @@
-public class Exercicio5 {
+import java.util.Scanner;
 
+public class Exercicio5 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma palavra: ");
+        String palavra = scanner.nextLine();
+
+
+        System.out.print("Digite uma letra: ");
+        char letra = scanner.next().charAt(0);
+
+        int ocorrencias = 0;
+
+        for(int i = 0; i < palavra.length(); i++){
+            if(palavra.charAt(i) == letra){
+                ocorrencias++;
+            }
+        }
+
+        System.out.println("Existem " + ocorrencias + " da letra " + letra + " na palavra " + palavra);
+
+    }
 }
