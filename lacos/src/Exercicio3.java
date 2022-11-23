7848ef8203d4f23ef0813cc6e1d0f90e0690f3b3
Gcolin1 <guilhermecolin09@gmail.com>
2022-11-23 15:22:30 -0300

    exercicios laços 5

--- lacos/src/Exercicio3.java
+++ lacos/src/Exercicio3.java
@@ -2,18 +2,15 @@ import java.util.Random;
 
 public class Exercicio3 {
     public static void main(String[] args){
-        int num = 3;
+        int num = Integer.parseInt(args[0]);
         int soma = 0;
 
         for (int i = 0; i < num; i++){
             Random numSorteio = new Random();
             int numeroSorteado = numSorteio.nextInt(1, 7);
             System.out.println("o numero " + numeroSorteado + " foi sorteado");
-
             soma += numeroSorteado;
        }
-
         System.out.println("A soma dos numeros sorteados é " + soma);
-
     }
 }
